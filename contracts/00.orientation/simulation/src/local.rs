8e2ecfce779cdf78ca81d37cfdf0847898f995bd
Willem Wyndham <willem@cs.umd.edu>
2020-12-01 16:05:15 -0500

    feat: add rust simulation test for first chapter
    Also small fixes and changed asconfig so that things will build

---
+++ contracts/00.orientation/simulation/src/local.rs
@@ -0,0 +1,12 @@
+use near_sdk::near_bindgen;
+#[allow(dead_code)]
+#[near_bindgen]
+pub struct Local {
+}
+
+#[near_bindgen]
+impl Local {
+  #[allow(unused_variables, dead_code)]
+  pub fn xcc(level: &str, account: &str, method: &str, args: &str){
+  }
+}
---
+++ contracts/00.orientation/simulation/src/remote.rs
@@ -0,0 +1,9 @@
+use near_sdk::near_bindgen;
+
+#[near_bindgen]
+pub struct Remote {}
+
+#[near_bindgen]
+impl Remote {
+  fn do_some_work() {}
+}
