81d1b05c2b41b0433377feefab4c591ea33c2377
BenKurrek <bkurrek@uwaterloo.ca>
2022-07-25 13:49:58 -0400

    added contract code

--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
-import { NearContract, NearBindgen, near, call, view } from 'near-sdk-js'
-import { isUndefined } from 'lodash-es'
+import { NearContract, NearBindgen, call, view, near, bytes } from "near-sdk-js";
+import { get_callback_result } from "./external";
+import { TGAS } from "./utils/constants";
+import { assert } from "./utils/assert";
 
 @NearBindgen
-class Counter extends NearContract {
-    constructor({ initial = 0 }) {
-        super()
-        this.count = initial
-    }
-
-    @call
-    increase({ n = 1 }) {
-        this.count += n
-        near.log(`Counter increased to ${this.count}`)
-    }
-
-    @call
-    decrease({ n }) {
-        // you can use default argument `n=1` too
-        // this is to illustrate a npm dependency: lodash can be used
-        if (isUndefined(n)) {
-            this.count -= 1
-        } else {
-            this.count -= n
-        }
-        near.log(`Counter decreased to ${this.count}`)
+class OnCall extends NearContract {
+    constructor({ account }) {
+        assert(near.currentAccountId() === near.predecessorAccountId(), "Method new is private");
+        super();
+        this.hello_account = account;
     }
 
     @view
-    getCount() {
-        return this.count
+    query_greeting() {
+        const promise = near.promiseBatchCreate("hello-near.test.near");
+        near.promiseBatchActionFunctionCall(promise, "get_greeting", bytes(JSON.stringify({})), 0, 5 * TGAS);
+        near.promiseThen(promise, near.currentAccountId(), "_query_greeting_callback", bytes(JSON.stringify({})), 0, 5 * TGAS);
+        return near.promiseReturn();
+    }
+
+    @call
+    _query_greeting_callback() {
+        const response = get_callback_result();
+        return response;
+    }
+
+    @call
+    change_greeting({ new_greeting }) {
+        const promise = near.promiseBatchCreate("hello-near.test.near");
+        near.promiseBatchActionFunctionCall(promise, "set_greeting", bytes(JSON.stringify({ message: new_greeting })), 0, 5 * TGAS);
+        near.promiseThen(promise, near.currentAccountId(), "change_greeting_callback", bytes(JSON.stringify({})), 0, 5 * TGAS);
+        return near.promiseReturn();
+    }
+
+    @call
+    change_greeting_callback() {
+        const response = get_callback_result();
+        return true;
     }
 }
-
