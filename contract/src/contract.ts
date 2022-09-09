d063de80be98aa031082498fb01ed74533e5d2e6
gagdiez <gagdiez.c@gmail.com>
2022-09-09 18:40:45 +0200

    updated sdk-js

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -1,43 +1,51 @@
-import { NearBindgen, initialize, call, near, bytes } from "near-sdk-js";
+import { NearBindgen, initialize, call, near, bytes, NearPromise } from "near-sdk-js";
 
-const TGAS = 10000000000000;
+const FIVE_TGAS = BigInt("50000000000000");
+const NO_DEPOSIT = BigInt(0);
+const NO_ARGS = bytes(JSON.stringify({}));
 
 @NearBindgen({})
 class CrossContractCall {
   hello_account: string = "hello-nearverse.testnet";
 
-  @initialize
+  @initialize({})
   init({ hello_account }: { hello_account: string }) {
     this.hello_account = hello_account
   }
 
-  @call
+  @call({})
   query_greeting() {
-    const call = near.promiseBatchCreate(this.hello_account);
-    near.promiseBatchActionFunctionCall(call, "get_greeting", bytes(JSON.stringify({})), 0, 5 * TGAS);
-    const then = near.promiseThen(call, near.currentAccountId(), "query_greeting_callback", bytes(JSON.stringify({})), 0, 5 * TGAS);
-    return near.promiseReturn(then);
+    const promise = NearPromise.new(this.hello_account)
+    .functionCall("get_greeting", NO_ARGS, NO_DEPOSIT, FIVE_TGAS)
+    .then(
+      NearPromise.new(near.currentAccountId())
+      .functionCall("query_greeting_callback", NO_ARGS, NO_DEPOSIT, FIVE_TGAS)
+    )
+    
+    return promise.asReturn();
   }
 
-  @call
+  @call({privateFunction: true})
   query_greeting_callback() {
-    assert(near.currentAccountId() === near.predecessorAccountId(), "This is a private method");
     const greeting = near.promiseResult(0) as String;
     return greeting.substring(1, greeting.length-1);
   }
 
-  @call
+  @call({})
   change_greeting({ new_greeting }: { new_greeting: string }) {
-    const call = near.promiseBatchCreate(this.hello_account);
-    near.promiseBatchActionFunctionCall(call, "set_greeting", bytes(JSON.stringify({ greeting: new_greeting })), 0, 5 * TGAS);
-    const then = near.promiseThen(call, near.currentAccountId(), "change_greeting_callback", bytes(JSON.stringify({})), 0, 5 * TGAS);
-    return near.promiseReturn(then);
+    const promise = NearPromise.new(this.hello_account)
+    .functionCall("set_greeting", bytes(JSON.stringify({ greeting: new_greeting })), NO_DEPOSIT, FIVE_TGAS)
+    .then(
+      NearPromise.new(near.currentAccountId())
+      .functionCall("change_greeting_callback", NO_ARGS, NO_DEPOSIT, FIVE_TGAS)
+    )
+
+    return promise.asReturn();
   }
 
-  @call
+  @call({privateFunction: true})
   change_greeting_callback() {
-    assert(near.currentAccountId() === near.predecessorAccountId(), "This is a private method");
-
+    
     if (near.promiseResultsCount() == BigInt(1)) {
       near.log("Promise was successful!")
       return true
@@ -47,5 +55,3 @@ class CrossContractCall {
     }
   }
 }
-
-function assert(condition, greeting) { if (!condition) throw Error(`Assertion failed: ${greeting}`); }
